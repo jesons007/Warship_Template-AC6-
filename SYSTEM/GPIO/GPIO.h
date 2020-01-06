fd489096261017d9ec21133946c1e1f1cc4cfc08
jesons007 <1815154258@qq.com>
2020-01-06 15:02:34 +0800

    GPIO Fast operate

--- SYSTEM/GPIO/GPIO.h
+++ SYSTEM/GPIO/GPIO.h
@@ -9,49 +9,25 @@
 #define INPUT_DOWN		0X8     //下拉输入
 #define INPUT_ANALOG    0X0	    //模拟输入
 
-//IO口操作宏定义
-#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
-#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
-#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
-//IO口地址映射
-#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
-#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
-#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
-#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
-#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
-#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
-#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
-
-#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
-#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
-#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
-#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
-#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
-#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
-#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
- 
+//////////////////////////////------------GPIO Fast operate------------///////////////////////////////////
 //IO口操作,只对单一的IO口!
 //确保n的值小于16!
-#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
-#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
+#define  PAout(n)   *((volatile unsigned long  *)(0X42210180+4*n))
+#define  PBout(n)   *((volatile unsigned long  *)(0X42218180+4*n))
+#define  PCout(n)   *((volatile unsigned long  *)(0X42220180+4*n))
+#define  PDout(n)   *((volatile unsigned long  *)(0X42228180+4*n))
+#define  PEout(n)   *((volatile unsigned long  *)(0X42230180+4*n))
+#define  PFout(n)   *((volatile unsigned long  *)(0X42238180+4*n))
+#define  PGout(n)   *((volatile unsigned long  *)(0X42240180+4*n))
 
-#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
-#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
 
-#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
-#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
-
-#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
-#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
-
-#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
-#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
-
-#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
-#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
-
-#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
-#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+#define  PAin(n)    *((volatile unsigned long  *)(0X42210100+4*n))
+#define  PBin(n)    *((volatile unsigned long  *)(0X42218100+4*n))
+#define  PCin(n)    *((volatile unsigned long  *)(0X42220100+4*n))
+#define  PDin(n)    *((volatile unsigned long  *)(0X42228100+4*n))
+#define  PEin(n)    *((volatile unsigned long  *)(0X42230100+4*n))
+#define  PFin(n)    *((volatile unsigned long  *)(0X42238100+4*n))
+#define  PGin(n)    *((volatile unsigned long  *)(0X42240100+4*n))
 /////////////////////////////////////////////////////////////////
 
 //GPIO引脚编号定义
